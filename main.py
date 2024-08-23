1e26bf1fa8894fe8d838a71be1ae6993ed265c9b
Kenny <tang.kenneth720@gmail.com>
2024-08-22 18:08:49 -0700

    Setup logging and env variable importing for discord token

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ import asyncio
 import os
 import sys
 import traceback
+import logging
 import sqlite3
+from dotenv import load_dotenv
 
 try:
-    import discord
+    import discord    
     from discord.ext import commands
-    # from dotenv import load_dotenv
-
+    
 # If failed to get any import, quit. This also stops the docker container
 except Exception as e:
     print(f"Error importing libraries: {e}")
@@ -36,56 +37,33 @@ Requirements:
 '''
 
 if __name__ == '__main__':
+
+    # Load environment variables from .env file
+    load_dotenv()
+    if not os.environ['DISCORD_TOKEN']:
+        raise Exception("DISCORD_TOKEN not found in .env file, please add it")
+    DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+    # Logging
+    if os.environ['LOGGING'] == 'TRUE' and os.environ['LOGGING_LEVEL']:
+        handler = logging.FileHandler(filename='dicebot.log', encoding='utf-8', mode='w')
+        discord.utils.setup_logging(handler=handler, root=False, level=getattr(logging, os.getenv("LOGGING_LEVEL")))
+
+
+
+    # intents = discord.Intents.default()
+    # intents.message_content = True
+
+    # client = discord.Client(intents=intents)
+
+    # @client.event
+    # async def on_ready():
+    #     print(f"We have logged in as {client.user}")
     
-    # Create the database in pwd. This returns a Connection object which represents the connection to the on-disk database
-    con = sqlite3.connect('dice_sets.db')
-    # Create a cursor which allows us to execute SQL statements and fetch results from SQL queries
-    cur = con.cursor()
-    # Create a table using the execute command. If already exists, dont create it and just use it.
-    cur.execute("CREATE TABLE IF NOT EXISTS movie(title, year, score)")
-    # Check that the table is created
-    res = cur.execute("SELECT name FROM sqlite_master")
-    # Print table name in the tuple that is returned
-    print(res.fetchone())
-    # Add stuff to the database
-    cur.execute("""
-        INSERT INTO movie VALUES
-                ('Monty Python and the Holy Grail', 1975, 8.2),
-                ('And Now for Something Completely Different', 1971, 7.5)
-    """)
-    # Save the entry to the database
-    con.commit()
-    # Check to see what scores we have
-    res = cur.execute("SELECT score FROM movie")
-    scores = res.fetchall()
-    for tup in scores:
-        print(tup[0])
-
-    # Insert more data, securely
-    data = [
-        ("Monty Python Live at the Hollywood Bowl", 1982, 7.9),
-        ("Monty Python's The Meaning of Life", 1983, 7.5),
-        ("Monty Python's Life of Brian", 1979, 8.0),
-    ]
-    # Make sure to use execute many
-    cur.executemany("INSERT INTO movie VALUES(?, ?, ?)", data)
-    con.commit()
-    # Make sure it was added
-    res = cur.execute("SELECT score FROM movie")
-    for row in cur.execute("SELECT year, title FROM movie ORDER BY year"):
-        print(row)
+    # @client.event
+    # async def on_message(message):
+    #     if message.author == client.user:
+    #         return
+    #     if message.content.startswith("$hello"):
+    #         await message.channel.send("Hello!")
     
-    # Close the database and open it again to test if it was written to disk
-    con.close()
-    new_con = sqlite3.connect('dice_sets.db')
-    new_cur = new_con.cursor()
-    res = new_cur.execute("SELECT title, year FROM movie ORDER BY score DESC")
-    title, year = res.fetchone()
-    print(f"{title} created in {year}")
-    new_con.close()
-
-
-
-
-
-
+    # client.run(DISCORD_TOKEN)
